f887e5b468b2eaa37bff39c15fc32d0e25b79195
hesham mamdouh <hisham1230456@gmail.com>
2022-03-11 15:12:27 +0200

    last phase 3

--- Quran_for_Deaf/settings.py
+++ Quran_for_Deaf/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-q2*a(*j$m_m*qtj%it(hw$9yys)+-xylk3d%g00zxwvcp(rzq1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.3']
+ALLOWED_HOSTS = []
 
 
 # Application definition
